90e9c7e609b9f51ece63558a4b305b75227fdbc6
Bvisi0n <jaro.de.graeve@outlook.com>
2025-12-27 11:32:06 +0100

    Add comments to window creation

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #ifndef UNICODE
-#define UNICODE             // Affects how text is interpreted.
+#define UNICODE             // Affects how text is interpreted
 #endif
-#define WIN32_LEAN_AND_MEAN // Cuts out the fat. (Exclude rarely used APIs)
+#define WIN32_LEAN_AND_MEAN // Cuts out the fat (Exclude rarely used APIs)
 #define NOMINMAX            // C++ Compatibility: Stop Windows from breaking std::min and std::max
 
 #include <windows.h>
@@ -15,21 +15,21 @@ int main()
 
 int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE, _In_ PWSTR, _In_ int nCmdShow)
 {
-    // Register the window class.
+    // Register the window class
     const wchar_t CLASS_NAME[] = L"Sample Window Class";
 
     WNDCLASS wc = { };
 
-    wc.lpfnWndProc = WindowProc;
-    wc.hInstance = hInstance;
-    wc.lpszClassName = CLASS_NAME;
+    wc.lpfnWndProc = WindowProc;    // Pointer to function
+    wc.hInstance = hInstance;       // Handle to application instance
+    wc.lpszClassName = CLASS_NAME;  // String that identifies window class
 
-    RegisterClass(&wc);
+    RegisterClass(&wc);             // Registers the window class with the operating system
 
-    // Create the window.
+    // Create the window
 
     HWND hwnd = CreateWindowEx(
-        0,                              // Optional window styles.
+        0,                              // Optional window styles
         CLASS_NAME,                     // Window class
         L"Learn to Program Windows",    // Window text
         WS_OVERLAPPEDWINDOW,            // Window style
@@ -45,12 +45,12 @@ int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE, _In_ PWSTR, _I
 
     if (hwnd == NULL)
     {
-        return 0;
+        return 0; // Failed to make a window -> terminate
     }
 
-    ShowWindow(hwnd, nCmdShow);
+    ShowWindow(hwnd, nCmdShow); // nCmdShow can be used to minimize or maximize a window
 
-    // Run the message loop.
+    // Run the message loop
 
     MSG msg = { };
     while (GetMessage(&msg, NULL, 0, 0) > 0)
@@ -75,7 +75,7 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
         PAINTSTRUCT ps;
         HDC hdc = BeginPaint(hwnd, &ps);
 
-        // All painting occurs here, between BeginPaint and EndPaint.
+        // All painting occurs here, between BeginPaint and EndPaint
 
         FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
 
