85949e570bacb4fddf43f4dda4b75af64bbfa1dd
Bvisi0n <jaro.de.graeve@outlook.com>
2026-01-05 15:08:24 +0100

    Comment clean-up in Config.h

--- include/Config.h
+++ include/Config.h
@@ -6,9 +6,9 @@
 	#define WIN32_LEAN_AND_MEAN // Cuts out the fat (Exclude rarely used APIs)
 	#define NOMINMAX            // C++ Compatibility: Stop Windows from breaking std::min and std::max
 
-	#include <d2d1.h> // Also has a min macro!
-	#include <windows.h>
-	#include <windowsx.h>
+	#include <windows.h>	// The core
+	#include <windowsx.h>	// The "helper" macros
+	#include <d2d1.h>		// Direct2D
 
 	// Linker instructions
 	#pragma comment(lib, "d2d1")
@@ -20,15 +20,15 @@
 		#define LOG_PRINT(...) ((void)0) 
 	#endif
 
-//TODO!: Project properties
-	//Under General
-		//Set C++ standard to C++23 for all builds
-	//Under C / C++ > General
-		//Set $(ProjectDir)include for all builds
-	//Under Linker > System
-		//Set Windows(/ SUBSYSTEM:WINDOWS) for Release builds
-		//Set Console(/ SUBSYSTEM:CONSOLE) for Debug builds
-	//Under Manifest Tool > Input and Output
-		//Set Per Monitor High DPI Aware for all builds
+// TODO!: Project properties
+	// Under General
+		// Set C++ standard to C++23 for all builds
+	// Under C / C++ > General
+		// Set $(ProjectDir)include for all builds
+	// Under Linker > System
+		// Set Windows(/ SUBSYSTEM:WINDOWS) for Release builds
+		// Set Console(/ SUBSYSTEM:CONSOLE) for Debug builds
+	// Under Manifest Tool > Input and Output
+		// Set Per Monitor High DPI Aware for all builds
 
 #endif
