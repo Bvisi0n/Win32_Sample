67e71dcecd928cabaadc6d6070099149652536f7
Bvisi0n <jaro.de.graeve@outlook.com>
2026-01-06 17:04:21 +0100

    Added concept to ID::ToHandle template
    Also defined a typedef for the underlying type so it can be changed easily across all enums and the concept.

--- include/ResourceIDs.h
+++ include/ResourceIDs.h
@@ -6,7 +6,9 @@
 
 namespace ID
 {	// Every WORD should be unique across all enumerations
-	enum class MenuBar : WORD
+	using UnderlyingType = WORD;
+
+	enum class MenuBar : UnderlyingType
 	{	// 0000 - 0099
 		AliceBlue			= 0000,
 		Lavender			= 0001,
@@ -14,20 +16,23 @@ namespace ID
 		PeachPuff			= 0003
 	};
 
-	enum class CursorModule : WORD
+	enum class CursorModule : UnderlyingType
 	{	// 0100 - 0109
 		ArrowRadioButton	= 0100,
 		HandRadioButton		= 0101,
 		CrossRadioButton	= 0102
 	};
 
-	enum class PopUpModule : WORD
+	enum class PopUpModule : UnderlyingType
 	{	// 0110 - 0119
 		ShowButton			= 0110,
 		Textbox				= 0111
 	};
 
 	template <typename T>
+	concept ResourceID = std::is_enum_v<T> && sizeof(T) <= sizeof(UnderlyingType);
+
+	template <ResourceID T>
 	constexpr HMENU ToHandle(T id)
 	{
 		return reinterpret_cast<HMENU>(static_cast<uintptr_t>(id));
